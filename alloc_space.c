e02b2a6c5d9dec5b947e88cb8cb9aa4bd5b79a93
Dylan Slogrove <dslogrove@gmail.com>
2019-06-25 14:09:54 +0200

    Wrote space-finding algorithm

---
+++ alloc_space.c
@@ -0,0 +1,18 @@
+#include "ft_malloc.h"
+
+void *alloc_space(t_allocation *alloc, const t_allocation area, const size_t alloc_len, const size_t new_size)
+{
+	void *start;
+	size_t i;
+
+	start = area.start;
+	i = 0;
+	while ((char *)start + new_size < (char *)area.start + area.size && i < alloc_len)
+	{
+		if ((unsigned)((char *)(alloc[i].start) - (char *)start) >= new_size)
+			return start;
+		start = (char *)(alloc[i].start) + alloc[i].size;
+		i++;
+	}
+	return NULL;
+}

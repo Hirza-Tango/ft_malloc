75c151acceac4bfae72210decf6d56a6226ad672
Dylan Slogrove <dslogrove@gmail.com>
2019-06-27 17:03:22 +0200

    Fixed printf issue in show_mem_alloc

--- show_alloc_mem.c
+++ show_alloc_mem.c
@@ -6,12 +6,12 @@
 /*   By: dslogrov <dslogrove@gmail.com>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/26 17:00:45 by dslogrov          #+#    #+#             */
-/*   Updated: 2019/06/26 17:00:47 by dslogrov         ###   ########.fr       */
+/*   Updated: 2019/06/27 16:56:22 by dslogrov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_malloc.h"
-#include <stdio.h>
+#include "libft.h"
 
 t_alloc g_alloc_tiny[ALLOC_NUM_TINY];
 t_alloc g_alloc_small[ALLOC_NUM_SMALL];
@@ -19,25 +19,33 @@ t_alloc g_alloc_large[ALLOC_NUM_LARGE];
 t_alloc g_region_small;
 t_alloc g_region_tiny;
 
+static void		show_range(t_alloc alloc)
+{
+		ft_putptr(alloc.start);
+		ft_putstr(" - ");
+		ft_putptr(alloc.start + alloc.size);
+		ft_putstr(" : ");
+		ft_put_uintmax_base(alloc.size, 10);
+		ft_putendl(" bytes");
+}
+
 static size_t	show_tiny(void)
 {
 	size_t	i;
 	size_t	ret;
-	t_alloc	current;
 	
-	printf("TINY : %p\n", g_region_tiny.start);
+	ft_putstr("TINY : ");
+	ft_putptr(g_region_tiny.start);
+	ft_putendl("");
 	sort_allocs((t_alloc *)g_alloc_tiny, ALLOC_NUM_TINY);
 	i = 0;
 	ret = 0;
 	while (i < ALLOC_NUM_TINY)
 	{
-		current = g_alloc_tiny[i++];
-		if (current.start != NULL)
-		{
-			printf("%p - %p : %zu bytes\n", current.start, (char *)
-				current.start + current.size, current.size);
-			ret += current.size;
-		}
+		if (g_alloc_tiny[i].start == NULL)
+			break ;
+		show_range(g_alloc_tiny[i]);
+		ret += g_alloc_tiny[i++].size;
 	}
 	return ret;
 }
@@ -46,21 +54,19 @@ static size_t	show_small(void)
 {
 	size_t	i;
 	size_t	ret;
-	t_alloc	current;
 	
-	printf("SMALL : %p\n", g_region_small.start);
+	ft_putstr("SMALL : ");
+	ft_putptr(g_region_small.start);
+	ft_putendl("");
 	sort_allocs((t_alloc *)g_alloc_small, ALLOC_NUM_SMALL);
 	i = 0;
 	ret = 0;
 	while (i < ALLOC_NUM_SMALL)
 	{
-		current = g_alloc_small[i++];
-		if (current.start != NULL)
-		{
-			printf("%p - %p : %zu bytes\n", current.start, (char *)
-				current.start + current.size, current.size);
-			ret += current.size;
-		}
+		if (g_alloc_small[i].start == NULL)
+			break ;
+		show_range(g_alloc_small[i]);
+		ret += g_alloc_small[i++].size;
 	}
 	return ret;
 }
@@ -69,21 +75,17 @@ static size_t	show_large(void)
 {
 	size_t	i;
 	size_t	ret;
-	t_alloc	current;
 	
-	printf("LARGE : \n");
+	ft_putendl("LARGE :");
 	sort_allocs((t_alloc *)g_alloc_large, ALLOC_NUM_LARGE);
 	i = 0;
 	ret = 0;
 	while (i < ALLOC_NUM_LARGE)
 	{
-		current = g_alloc_large[i++];
-		if (current.start != NULL)
-		{
-			printf("%p - %p : %zu bytes\n", current.start, (char *)
-				current.start + current.size, current.size);
-			ret += current.size;
-		}
+		if (g_alloc_large[i].start == NULL)
+			break ;
+		show_range(g_alloc_large[i]);
+		ret += g_alloc_large[i++].size;
 	}
 	return ret;
 }
@@ -94,10 +96,7 @@ void			show_alloc_mem(void)
 	size_t	size;
 
 	size = show_tiny() + show_small() + show_large();
-	printf("Total : %zu bytes\n", size);
-}
-
-void			show_alloc_mem_ex(void)
-{
-
+	ft_putstr("Total : ");
+	ft_putnbr(size);
+	ft_putendl("bytes");
 }

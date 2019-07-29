ce6843b36cdf3d9d5cea268c5ff84fa44024b73f
Dylan Slogrove <dslogrove@gmail.com>
2019-07-29 17:15:09 +0200

    Changed back to DYLD env variables. Someday, someday...

--- tests/run.sh
+++ tests/run.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-export LD_LIBRARY_PATH=.
-export LD_INSERT_LIBRARIES="libft_malloc.so"
-export LD_FORCE_FLAT_NAMESPACE=1
+export DYLD_LIBRARY_PATH=.
+export DYLD_INSERT_LIBRARIES="libft_malloc.so"
+export DYLD_FORCE_FLAT_NAMESPACE=1
 $@
